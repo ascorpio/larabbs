6fd406429b117ad2c7b7f11a9c99449c7bf61d40
ascorpio <ascorpio@qq.com>
2021-06-05 14:49:18 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Route::get('/', "pagesController@root")->name('root');
 Auth::routes(['verify' => true]);
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
