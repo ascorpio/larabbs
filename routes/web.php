556c45ac96ce37e242c3297cb0fe71848d937e52
ascorpio <ascorpio@qq.com>
2021-06-18 14:52:12 +0800

    消息通知列表

--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,10 @@ Route::get('/', "pagesController@root")->name('root');
 
 // 该路由定义在此处 vendor/laravel/ui/src/AuthRouteMethods.php
 Auth::routes(['verify' => true]);
-
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
-
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
-
 Route::resource('categories', 'CategoriesController', ['only' => 'show']);
-
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
-
 Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
-
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
+Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
