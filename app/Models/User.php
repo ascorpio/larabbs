21dc6fc7266034683cb0d03ba9e4b237dfe7ce2c
ascorpio <ascorpio@qq.com>
2021-06-22 10:38:37 +0800

    活跃用户

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Traits\ActiveUserHelper;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -12,7 +13,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasFactory, Notifiable, MustVerifyEmailTrait, HasRoles;
+    use HasFactory, Notifiable, MustVerifyEmailTrait, HasRoles, ActiveUserHelper;
 
     use Notifiable {
         notify as protected laravelNotify;
