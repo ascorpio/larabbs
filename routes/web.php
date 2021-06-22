e0f03c0b781b85d639502cf23364c4dd892e0ba1
ascorpio <ascorpio@qq.com>
2021-06-22 14:46:04 +0800

    首页话题列表

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', "pagesController@root")->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 // 该路由定义在此处 vendor/laravel/ui/src/AuthRouteMethods.php
 Auth::routes(['verify' => true]);
